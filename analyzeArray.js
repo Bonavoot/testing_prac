344af0f3a5eb12e3f4a3630f8b4638fbe3528a6d
Bonavoot <Jbonventre95@gmail.com>
2022-04-08 06:16:16 -0400

    analyzeArray test

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,6 +1,9 @@
 function analyzeArray(arr) {
   return {
     average: arr.reduce((prev, curr) => prev + curr) / arr.length,
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+    length: arr.length,
   };
 }
 
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,6 +1,9 @@
 const analyzeArray = require("./analyzeArray");
 const object = {
   average: 4,
+  min: 1,
+  max: 8,
+  length: 6,
 };
 
 test("average", () => {
